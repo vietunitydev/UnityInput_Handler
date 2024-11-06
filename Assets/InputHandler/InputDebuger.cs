14379b14f80aa8b7b8da74660748389e67ac736a
viet <vietunitydev2222@gmail.com>
2024-11-06 17:49:09 +0700

    - fix

--- Assets/InputHandler/InputDebuger.cs
+++ Assets/InputHandler/InputDebuger.cs
@@ -14,21 +14,29 @@ public class InputDebuger : MonoBehaviour
     [SerializeField] private float dpad_X;
     [SerializeField] private float dpad_Y;
 
+    [SerializeField] private int playerNumber;
+
     private void Update()
     {
-        right_X = instance.GetAxisValue(GamepadAxis.RightHorizontal, 1);
-        right_Y = instance.GetAxisValue(GamepadAxis.RightVertical, 1);
+        right_X = instance.GetAxisValue(GamepadAxis.RightHorizontal, playerNumber);
+        right_Y = instance.GetAxisValue(GamepadAxis.RightVertical, playerNumber);
         
-        left_X = instance.GetAxisValue(GamepadAxis.LeftHorizontal, 1);
-        left_Y = instance.GetAxisValue(GamepadAxis.LeftVertical, 1);
+        left_X = instance.GetAxisValue(GamepadAxis.LeftHorizontal, playerNumber);
+        left_Y = instance.GetAxisValue(GamepadAxis.LeftVertical, playerNumber);
         
-        dpad_X = instance.GetAxisValue(GamepadAxis.LeftHorizontal, 1);
-        dpad_Y = instance.GetAxisValue(GamepadAxis.LeftVertical, 1);
+        dpad_X = instance.GetAxisValue(GamepadAxis.LeftHorizontal, playerNumber);
+        dpad_Y = instance.GetAxisValue(GamepadAxis.LeftVertical, playerNumber);
+        
+        var ActionEast = instance.GetButton(GamepadButton.ActionEast, playerNumber);
+        var ActionNorth = instance.GetButton(GamepadButton.ActionNorth, playerNumber);
+        var ActionWest = instance.GetButton(GamepadButton.ActionWest, playerNumber);
+        var ActionSouth = instance.GetButton(GamepadButton.ActionSouth, playerNumber);
 
-        var x = instance.GetButton(GamepadButton.Start, 1);
-        var y = instance.GetButton(GamepadButton.BackSelect, 1);
+        var x = instance.GetButton(GamepadButton.Start, playerNumber);
+        var y = instance.GetButton(GamepadButton.BackSelect, playerNumber);
 
-        Debug.Log($"x {x} == y {y}");
+        Debug.Log($"x {x} == y {y} ");
+        Debug.Log($"ActionEast {ActionEast} == ActionNorth {ActionNorth} == ActionWest {ActionWest} == ActionNorth {ActionNorth} ");
         
         
     }
